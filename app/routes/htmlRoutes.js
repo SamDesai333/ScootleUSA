e2e7bab57d252f75790dfd3041ab91ff230220d4
Samidh Desai <samidhdesai@gmail.com>
2018-04-18 21:16:09 -0500

    adds the ye page

--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -10,4 +10,13 @@ module.exports = function(app) {
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
+
+  app.get("/about", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/about.html"));
+  });
+
+  app.get("/young", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/young.html"));
+  });
+  
 };
